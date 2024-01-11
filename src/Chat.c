66b702b0c183ec93b8f3b279446677cb4cfa2e3c
Ayman <87582743+mastergive514@users.noreply.github.com>
2024-01-11 22:05:14 +0100

    Update Chat.c

--- src/Chat.c
+++ src/Chat.c
@@ -831,7 +831,10 @@ static void SphereCommand_DoSphere(void) {
 
     if (sphere_block == -1) toPlace = Inventory_SelectedBlock;
 
-    Vec3 curPos = Game_LocalPlayer.Position;
+    Vec3 curPos;
+    Vec3.X = localPlayer->Base.Position.X;
+    Vec3.Y = localPlayer->Base.Position.Y;
+    Vec3.Z = localPlayer->Base.Position.Z;
 
     for (y = (int)(curPos.Y) - sphere_radius; y <= (int)(curPos.Y) + sphere_radius; y++) {
         for (z = (int)(curPos.Z) - sphere_radius; z <= (int)(curPos.Z) + sphere_radius; z++) {
